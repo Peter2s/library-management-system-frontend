7878a1d7b775bf09033ce70389a66fbe03a76663
3bdElsamea <asayed4761@gmail.com>
2023-04-14 16:18:22 +0200

    Super Admin & Employee

--- src/app/administration/employees/get-employee/get-employee.component.ts
+++ src/app/administration/employees/get-employee/get-employee.component.ts
@@ -1,10 +1,32 @@
-import { Component } from '@angular/core';
-
+import { Component } from "@angular/core";
+import { IManagers } from "../../../models/IManagers";
+import { LoadingService } from "../../../shared/services/loading.service";
+import { ActivatedRoute, Router } from "@angular/router";
+import { Subscription } from "rxjs";
+import { EmployeesService } from "../../services/employees.service";
 @Component({
-  selector: 'app-get-employee',
-  templateUrl: './get-employee.component.html',
-  styleUrls: ['./get-employee.component.css']
+  selector: "app-get-employee",
+  templateUrl: "./get-employee.component.html",
+  styleUrls: ["./get-employee.component.css"],
 })
 export class GetEmployeeComponent {
-
+  employee: IManagers | undefined;
+  fullName: string = "";
+  subscription: Subscription[] = [];
+  constructor(
+    public loadingService: LoadingService,
+    public route: ActivatedRoute,
+    public router: Router,
+    private employeesService: EmployeesService
+  ) {}
+  ngOnInit() {
+    const id = this.route.snapshot.params["id"];
+    const url = `/employee/${id}`;
+    const sub = this.employeesService.getEmployeeById(url).subscribe((data) => {
+      this.employee = data.data;
+      this.fullName =
+        `${this?.employee.firstName[0]}.${this.employee?.lastName}` || "";
+      this.subscription.push(sub);
+    });
+  }
 }
